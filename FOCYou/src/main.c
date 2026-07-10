86f3b2331a1c1d3c30323b2ca4449b96fa7b46c2
andrevale99 <andrefvale@gmail.com>
2026-07-10 17:46:40 -0300

    .

--- FOCYou/src/main.c
+++ FOCYou/src/main.c
@@ -4,12 +4,11 @@
 
 #include <arm_math.h>
 
-#include "init_rcc.h"
+#include "init_lcd16x2.h"
 #include "init_systick.h"
 
+#include "rcc.h"
 #include "lcd16x2.h"
-#include "init_lcd16x2.h"
-
 #include "inversor.h"
 
 const lcd16x2_handle lcd = {
@@ -34,12 +33,23 @@ inversor_t inv = {
 
 int main(void)
 {
+    rcc_clk_enable(RCC_CLK_HSE, true);
+    rcc_switch_clk_system(RCC_CLK_HSE);
+    rcc_AHB_set_prescale(AHB_DIV_1);
 
-    init_rcc();
     init_systick();
+
+    lcd16x2_init_4bits(&lcd, init_periferico_lcd16x2);
+    lcd16x2_send_cmd(&lcd, DISPLAY_ON | CURSOR_ON);
 
     inversor_init(&inv);
     inversor_set_duty(&inv, 1150, 40, 1);
+
+    char buffer[16];
+    int size = 0;
+
+    size = sprintf(buffer, "%ldkHz", inversor_get_frequency(&inv));
+    lcd16x2_write_string(&lcd, buffer, size);
 
     while (1)
     {
