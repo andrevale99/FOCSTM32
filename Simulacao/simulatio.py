ce9e3978060c6e780534cffb1df71df48425a809
andrevale99 <andrefvale@gmail.com>
2026-07-14 15:59:03 -0300

    .

--- Simulacao/simulatio.py
+++ Simulacao/simulatio.py
@@ -13,52 +13,51 @@ print(os.listdir(PATH))
 # =====================================================================
 
 # Resistencia de armadura
-Rs = 0.386 #Ohm
+Rs = 0.6 #Ohm
 
 # Indutancia de magnetizacao
-L = 6.53e-5 #H
+L = 5e-3 #H
 
 # Constantes eletricas e mecanicas
 Ke = 0.0276
 Kt = Ke
 
 # Torque da carga
-Tl = 0.1
+Tl = 0.0
 
 # Coeficiente de amortecimento
-B = 6.75e-6
+B = 7.312e-7
 
 # Momento de inercia
-J = 3.33e-6
+J = 7.312e-6
 
 # Quantidade de Polos no motor
-POLOS = 4
+POLOS = 14
 PARES_DE_POLOS = POLOS / 2
 
 # Amplitude da rede
-Vm = 48*0.8 #V
+Vm = 12 #V
 
 # Defasagens das fases
 PHI_A = 0
 PHI_B = -2*pi/3
 PHI_C = 2*pi/3
 
-TEMPO_MAX = 0.1     # s
-dt = 1e-5         # s
+ti = 0.0
+tf = 0.2     # s
+dt = 1e-4         # s
 
 bldc1 = bldc(Rs,L,B,J,Ke,Kt,PARES_DE_POLOS,Vm)
 
-time,Vabc,_,iabc,Te,omega_r,_ = bldc1.simulation_open_loop(t0=0,tf=0.1,dt=1e-4,
-                                                           Tl=0.1, 
-                                                           back_emf_trapezoidal_flag=False)
+time,Vabc,eabc,iabc,Te,omega_r,_ = bldc1.simulation_open_loop(t0=ti,tf=tf,dt=dt,
+                                                           Tl=Tl, 
+                                                           back_emf_trapezoidal_flag=True)
 
 plt.figure(figsize=(12, 10))
 plt.subplot(411)
 
 plt.title("Vabc")
-plt.plot(time, Vabc[0], label="Va")
-plt.plot(time, Vabc[1], label="Vb")
-plt.plot(time, Vabc[2], label="Vc")
+plt.plot(time, eabc.T,label=["ea","eb","ec"])
 
 plt.grid()
 plt.legend()
@@ -66,9 +65,7 @@ plt.legend()
 plt.subplot(412)
 
 plt.title("Iabc")
-plt.plot(time, iabc[0], label="ia")
-plt.plot(time, iabc[1], label="ib")
-plt.plot(time, iabc[2], label="ic")
+plt.plot(time, iabc.T, label=["ia","ib","ic"])
 
 plt.grid()
 plt.legend()
