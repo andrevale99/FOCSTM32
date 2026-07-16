1aa2bf75d8933d1498429b4936129bdd5e61dc3a
andrevale99 <andrefvale@gmail.com>
2026-07-16 16:06:54 -0300

    quero terminar essa bagaça

--- Simulacao/python/bldc.py
+++ Simulacao/python/bldc.py
@@ -30,6 +30,12 @@ def Clarke_Inverse_Transform(xalpha, xbeta):
     )
     return np.array([xa, xb, xc])
 
+def rads_to_rpm(omega):
+    return omega * 60 / (2*pi)
+    
+def rpm_to_rads(rpm):
+    return rpm * 2*pi/60
+
 class BLDC():
 
     def __init__(self, R, L, B, J, Ke, Kt, P, Vdc):
@@ -44,12 +50,8 @@ class BLDC():
         self.Vdc = Vdc
 
         self.PHI_A = 0
-        self.PHI_B = -2 * pi / 3
-        self.PHI_C = 2 * pi / 3
-
-        # ============================================================
-        # ESTADOS DO MOTOR
-        # ============================================================
+        self.PHI_B = 2 * pi / 3
+        self.PHI_C = -2 * pi / 3
 
         self.ia = 0.0
         self.ib = 0.0
@@ -60,283 +62,79 @@ class BLDC():
 
         self.Te = 0.0
 
-    def rads_to_rpm(self, omega):
-        return omega * 60 / (2*pi)
-    
-    def rpm_to_rads(self,rpm):
-        return rpm * 2*pi/60
-
-    def simulation_open_loop(self, t0, tf, dt=1e-5, Tl=0, back_emf_trapezoidal_flag=True):
-        time = np.arange(t0, tf, dt)
-        N = len(time)
-
-        theta_r = np.zeros(N)
-        omega_r = np.zeros(N)
-
-        ia = np.zeros(N)
-        ib = np.zeros(N)
-        ic = np.zeros(N)
-
-        Te = np.zeros(N)
-
-        Va = np.zeros(N)
-        Vb = np.zeros(N)
-        Vc = np.zeros(N)
-
-        ea = np.zeros(N)
-        eb = np.zeros(N)
-        ec = np.zeros(N)
-
-        for k in range(N-1):
-
-            # Ângulo e velocidade elétrica do rotor
-            theta_e = self.P * theta_r[k]
-
-            # Tensões trifásicas da alimentação
-            Va[k] = self.Vdc * np.sin(theta_e  + self.PHI_A)
-            Vb[k] = self.Vdc * np.sin(theta_e  + self.PHI_B)
-            Vc[k] = self.Vdc * np.sin(theta_e  + self.PHI_C)
-
-            # Back-EMF
-            if not back_emf_trapezoidal_flag: 
-                fa = np.sin(theta_e + self.PHI_A)
-                fb = np.sin(theta_e + self.PHI_B)
-                fc = np.sin(theta_e + self.PHI_C)
-            else:
-                fa = self.back_emf_trapezoidal(theta_e + self.PHI_A)
-                fb = self.back_emf_trapezoidal(theta_e + self.PHI_B)
-                fc = self.back_emf_trapezoidal(theta_e + self.PHI_C)
-
-
-            ea[k] = self.Ke * omega_r[k] * fa
-            eb[k] = self.Ke * omega_r[k] * fb
-            ec[k] = self.Ke * omega_r[k] * fc
-    
-            # Derivadas das correntes
-            dia = (Va[k] - self.R * ia[k] - ea[k]) / self.L
-            dib = (Vb[k] - self.R * ib[k] - eb[k]) / self.L
-            dic = (Vc[k] - self.R * ic[k] - ec[k]) / self.L
-
-            # Integração (Euler)
-            ia[k+1] = ia[k] + dia * dt
-            ib[k+1] = ib[k] + dib * dt
-            ic[k+1] = ic[k] + dic * dt
-
-            # Torque eletromagnético
-            Te[k] = self.Kt * (
-                ia[k] * fa +
-                ib[k] * fb +
-                ic[k] * fc
-            )
-
-            # Dinâmica mecânica
-            domega = (Te[k] - Tl - self.B * omega_r[k]) / self.J
-
-            omega_r[k+1] = omega_r[k] + domega * dt
-            theta_r[k+1] = theta_r[k] + omega_r[k+1] * dt
-
-        return [time, np.array([Va,Vb,Vc], dtype=np.float32), np.array([ea,eb,ec], dtype=np.float32),
-                np.array([ia,ib,ic], dtype=np.float32), Te, omega_r, theta_r]
-
-    def step(self, Va, Vb, Vc, Tl=0.0, dt=1e-5):
-        """
-        Executa um passo da simulação do motor BLDC.
-
-        Parameters
-        ----------
-        Va, Vb, Vc : float
-            Tensões instantâneas aplicadas às fases A, B e C [V].
-
-        Tl : float
-            Torque de carga [N.m].
-
-        dt : float
-            Passo de integração [s].
-
-        Returns
-        -------
-        dict
-            Estados atualizados do motor.
-        """
-
-        # ============================================================
-        # 1. ÂNGULO ELÉTRICO
-        # ============================================================
+    def step(self, Va, Vb, Vc, Tl=0.0, dt=1e-5, back_emf_trapezoidal_flag=True):
 
         theta_e = self.P * self.theta_r
+        theta_e = np.mod(theta_e, 2 * np.pi)
 
-        theta_e = np.mod(
-            theta_e,
-            2 * np.pi
-        )
+        if back_emf_trapezoidal_flag:
+            fa = self.back_emf_trapezoidal(theta_e + self.PHI_A)
+            fb = self.back_emf_trapezoidal(theta_e + self.PHI_B)
+            fc = self.back_emf_trapezoidal(theta_e + self.PHI_C)
+        else:
+            fa = np.sin(theta_e + self.PHI_A)
+            fb = np.sin(theta_e + self.PHI_B)
+            fc = np.sin(theta_e + self.PHI_C)
 
+        omega_e = self.P * self.omega_r
 
-        # ============================================================
-        # 2. FUNÇÕES DA FORÇA CONTRAELETROMOTRIZ
-        # ============================================================
+        ea = self.Ke * omega_e * fa
+        eb = self.Ke * omega_e * fb
+        ec = self.Ke * omega_e * fc
 
-        fa = self.back_emf_trapezoidal(
-            theta_e + self.PHI_A
-        )
-
-        fb = self.back_emf_trapezoidal(
-            theta_e + self.PHI_B
-        )
-
-        fc = self.back_emf_trapezoidal(
-            theta_e + self.PHI_C
-        )
-
-
-        # ============================================================
-        # 3. FORÇAS CONTRAELETROMOTRIZES
-        # ============================================================
-
-        ea = self.Ke * self.omega_r * fa
-
-        eb = self.Ke * self.omega_r * fb
-
-        ec = self.Ke * self.omega_r * fc
-
-
-        # ============================================================
-        # 4. DERIVADAS DAS CORRENTES
-        # ============================================================
-
-        dia = (
-            Va
-            - self.R * self.ia
-            - ea
-        ) / self.L
-
-        dib = (
-            Vb
-            - self.R * self.ib
-            - eb
-        ) / self.L
-
-        dic = (
-            Vc
-            - self.R * self.ic
-            - ec
-        ) / self.L
-
-
-        # ============================================================
-        # 5. INTEGRAÇÃO DAS CORRENTES
-        # ============================================================
+        dia = (Va - self.R * self.ia - ea) / self.L
+        dib = (Vb - self.R * self.ib - eb) / self.L
+        dic = (Vc - self.R * self.ic - ec) / self.L
 
         self.ia += dia * dt
-
         self.ib += dib * dt
-
         self.ic += dic * dt
 
+        self.Te = self.Kt * (self.ia * fa + self.ib * fb + self.ic * fc)
 
-        # ============================================================
-        # 6. TORQUE ELETROMAGNÉTICO
-        # ============================================================
-
-        self.Te = self.Kt * (
-
-            self.ia * fa
-            + self.ib * fb
-            + self.ic * fc
-
-        )
-
-
-        # ============================================================
-        # 7. DINÂMICA MECÂNICA
-        # ============================================================
-
-        domega = (
-
-            self.Te
-            - Tl
-            - self.B * self.omega_r
-
-        ) / self.J
-
-
-        # ============================================================
-        # 8. INTEGRAÇÃO DA VELOCIDADE
-        # ============================================================
+        domega = (self.Te - Tl - self.B * self.omega_r) / self.J
 
         self.omega_r += domega * dt
-
-
-        # ============================================================
-        # 9. INTEGRAÇÃO DA POSIÇÃO
-        # ============================================================
-
         self.theta_r += self.omega_r * dt
 
-
-        # ============================================================
-        # 10. RETORNO DOS ESTADOS
-        # ============================================================
-
         return {
-
-            "ia": self.ia,
-            "ib": self.ib,
-            "ic": self.ic,
-
-            "ea": ea,
-            "eb": eb,
-            "ec": ec,
-
+            "ia": self.ia, "ib": self.ib, "ic": self.ic,
+            "ea": ea, "eb": eb, "ec": ec,
             "Te": self.Te,
-
             "omega_r": self.omega_r,
-
             "theta_r": self.theta_r,
-
             "theta_e": theta_e
-
         }
 
-    def back_emf_trapezoidal(self,theta):
-        '''
-        Back-EMF trapezoidal normalizada (-1 a +1).
-        theta em radianos.
-        '''
+    def back_emf_trapezoidal(self, theta):
         theta = np.mod(theta, 2*np.pi)
-
         if theta < np.pi/6:
             return 6*theta/np.pi
-
         elif theta < 5*np.pi/6:
             return 1.0
-
         elif theta < 7*np.pi/6:
             return 1 - 6*(theta-5*np.pi/6)/np.pi
-
         elif theta < 11*np.pi/6:
             return -1.0
-
         else:
             return -1 + 6*(theta-11*np.pi/6)/np.pi
 
     def Clarke(self, xabc):
-        return Clarke_Transform(xabc[0],xabc[1],xabc[2])
-    
+        return Clarke_Transform(xabc[0], xabc[1], xabc[2])
+
     def ClarkeInverse(self, xalphabeta):
         return Clarke_Inverse_Transform(xalphabeta[0], xalphabeta[1])
-    
+
     def Park(self, xalphabeta, theta):
         return Park_Transform(xalphabeta[0], xalphabeta[1], theta)
 
     def ParkInverse(self, xdq, theta):
-        return Park_Inverse_Transform(xdq[0],xdq[1], theta)
-    
+        return Park_Inverse_Transform(xdq[0], xdq[1], theta)
+
     def set_initial_conditions(self):
         self.ia = 0.0
         self.ib = 0.0
         self.ic = 0.0
-
         self.omega_r = 0.0001
         self.theta_r = 0.0001
-
         self.Te = 0.0
--- Simulacao/python/simulation_closedloop.py
+++ Simulacao/python/simulation_closedloop.py
@@ -6,7 +6,7 @@ import os
 
 from PIcontroller import PIController
 from Inverter import Inverter
-from bldc import BLDC
+from bldc import BLDC, rpm_to_rads
 from SVPWM import SVPWM
 
 PATH = os.getcwd()
@@ -32,54 +32,49 @@ def cm_to_inches(cm):
 # Amplitude da rede
 Vdc = 24 #V
 
-# Defasagens das fases
-PHI_A = 0
-PHI_B = -2*pi/3
-PHI_C = 2*pi/3
-
 # ========================================= 
 #   PARAMETROS MOTOR
 # =========================================
 
 # Resistencia de armadura
-Rs = 0.3 #Ohm
+Rs = 0.6 #Ohm
 
 # Indutancia de magnetizacao
-L = 5e-4 #H
+L = 2e-4 #H
 
 # Constantes eletricas e mecanicas
-Ke = 0.0676
-Kt = 0.06
+Ke = 0.05
+Kt = Ke
 
 # Torque da carga
 Tl = 0.1
 
 # Coeficiente de amortecimento
-B = 1.312e-7
+B = 2e-3
 
 # Momento de inercia
-J = 4.6e-6
+J = 1e-4
 
 # Quantidade de Polos no motor
-POLOS = 14
+POLOS = 8
 PARES_DE_POLOS = POLOS / 2
 
 # ========================================= 
 #   PARAMETROS DOS CONTROLADORES
 # =========================================
 
-VDC_MAX = 24
-VDC_MIN = -24
+VDC_MAX = 12
+VDC_MIN = -VDC_MAX
 
-RPM_MAX = 1500
-RPM_MIN = -RPM_MAX
+PI_IQ_MAX = 20
+PI_IQ_MIN = -PI_IQ_MAX
 
 # ========================================= 
 #   PARAMETROS SIMULACAO
 # =========================================
 
 ti = 0.0 #s
-tf = 0.2 #s
+tf = 0.5 #s
 dt = 1e-5 #s
 
 time = np.arange(
@@ -98,9 +93,10 @@ motor = BLDC(Rs,L,B,J,Ke,Kt,PARES_DE_POLOS,Vdc)
 pwm = SVPWM(Hz=10000, Vdc=Vdc)
 inverter = Inverter(Vdc=Vdc)
 
-pi_omega = PIController(Kp=1.5,Ki=0.1,Ts=dt, output_min=RPM_MAX, output_max=0)
-pi_d = PIController(Kp=5,Ki=1.8,Ts=dt, output_min=VDC_MIN, output_max=VDC_MAX)
-pi_q = PIController(Kp=5,Ki=.5,Ts=dt, output_min=VDC_MIN, output_max=VDC_MAX)
+pi_omega = PIController(Kp=1,Ki=0.1,Ts=dt, 
+                        output_min=PI_IQ_MIN, output_max=PI_IQ_MAX)
+pi_d = PIController(Kp=2,Ki=0.2,Ts=dt, output_min=VDC_MIN, output_max=VDC_MAX)
+pi_q = PIController(Kp=2,Ki=0.2,Ts=dt, output_min=VDC_MIN, output_max=VDC_MAX)
 
 # ========================================= 
 #   SIMULACAO
@@ -108,11 +104,12 @@ pi_q = PIController(Kp=5,Ki=.5,Ts=dt, output_min=VDC_MIN, output_max=VDC_MAX)
 
 motor.set_initial_conditions()
 
-# rpm_ref = 1000 
-# omega_ref = motor.rpm_to_rads(rpm_ref)
+# iq_ref = 4
+id_ref = 0     # Em motores de ímãs permanentes, d-axis ref é geralmente 0
 
-iq_ref = 1
-id_ref = 5
+rpm_ref = 50  # Exemplo: 800 RPM
+omega_ref = rpm_to_rads(rpm_ref)
+print(omega_ref)
 
 log_omega = np.zeros(N)
 log_Vabc = np.zeros((N,3))
@@ -120,11 +117,18 @@ log_iabc = np.zeros((N,3))
 log_idq = np.zeros((N,2))
 log_Te = np.zeros(N)
 log_theta = np.zeros(N)
+log_iq_ref = np.zeros(N)
+log_duties = np.zeros((N,3))
+log_theta = np.zeros(N)
 
 for k in range(N):
     # A. Medição (feedback do modelo)
     # Suponha que 'motor' já foi instanciado e 'theta_r' existe no estado
     theta_e = motor.theta_r * motor.P 
+    omega_e = motor.omega_r * motor.P # Velocidade elétrica
+
+    iq_ref = pi_omega.update(omega_ref, motor.omega_r)
+
     
     # B. Transformada de Clarke e Park
     i_alpha, i_beta = motor.Clarke([motor.ia, motor.ib, motor.ic])
@@ -149,17 +153,20 @@ for k in range(N):
     v_abc = inverter.output_voltage(duties[0], duties[1], duties[2])
 
     # H. Atualização da Planta (Motor)
-    estados = motor.step(v_abc[0], v_abc[1], v_abc[2], Tl=Tl, dt=dt)
+    estados = motor.step(v_abc[0], v_abc[1], v_abc[2], Tl=Tl, dt=dt, 
+                         back_emf_trapezoidal_flag=False)
 
     log_omega[k] = estados['omega_r']
     log_theta[k] = estados['theta_r']
     log_iabc[k,0] = estados["ia"]
     log_iabc[k,1] = estados["ib"]
     log_iabc[k,2] = estados["ic"]
+    log_theta[k] = estados["theta_e"]
     log_Te[k] = estados["Te"]
     log_Vabc[k] = v_abc
     log_idq[k,0] = i_d
     log_idq[k,1] = i_q
+    log_duties[k] = duties
 
 plt.figure(figsize=(cm_to_inches(25),cm_to_inches(20)))
 
