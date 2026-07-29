4d93fa4ae0097f27305731872fa92a1c62d50d3f
andrevale99 <andrefvale@gmail.com>
2026-07-29 09:53:13 -0300

    .

--- Simulacao/ganhos.py
+++ Simulacao/ganhos.py
@@ -1,16 +1,17 @@
 import numpy as np
 
-R  = 0.161           # Ohm  - resistencia de armadura
-L  = 0.052e-3         # H    - indutancia de magnetizacao
-M  = 0.0            # H    - indutancia mutua
-Ke = 0.0255     # V/(rad/s) - constante eletrica (2.67 V/krpm)
-J  = 43.3e-3         # kg.m^2 - momento de inercia
-B  = 1.2e-6         # coeficiente de amortecimento
-Tl = 0.0            # N.m  - torque de carga
-P  = 8             # numero de pares de polos
-Kt = 0.0255     # N.m/A - constante de torque
+R  = 0.5              # Ohm       - resistencia de armadura
+L  = 0.3e-3           # H         - indutancia de magnetizacao
+M  = 0.0              # H         - indutancia mutua (nao usada em bldc_step)
+Ke = 0.00909483       # V/(rad/s) - constante eletrica (FCEM)
+J  = 1.0e-4           # kg.m^2    - momento de inercia do rotor
+B  = 5e-5             # N.m/(rad/s) - coeficiente de atrito viscoso
+Tl = 0.0              # N.m       - torque de carga aplicado ao eixo
+P  = 14               # -         - numero de pares de polos
+Kt = 0.00909483       # N.m/A     - constante de torque
 
-omegacc = 6200
+
+omegacc = 3141
 
 Kpq = L * omegacc
 Kiq = R * omegacc
