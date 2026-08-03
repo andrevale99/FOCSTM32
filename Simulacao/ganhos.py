412f025020f088e6b929b69ae1bea607a0f77524
andrevale99 <andrefvale@gmail.com>
2026-08-03 15:01:53 -0300

    Correção do cálculo dos valores de ganho da malha de velocidade

--- Simulacao/ganhos.py
+++ Simulacao/ganhos.py
@@ -1,27 +1,36 @@
 import numpy as np
 
-R  = 0.5              # Ohm       - resistencia de armadura
-L  = 0.3e-3           # H         - indutancia de magnetizacao
-M  = 0.0              # H         - indutancia mutua (nao usada em bldc_step)
-Ke = 0.00909483       # V/(rad/s) - constante eletrica (FCEM)
-J  = 1.0e-4           # kg.m^2    - momento de inercia do rotor
-B  = 5e-5             # N.m/(rad/s) - coeficiente de atrito viscoso
-Tl = 0.0              # N.m       - torque de carga aplicado ao eixo
-P  = 14               # -         - numero de pares de polos
-Kt = 0.00909483       # N.m/A     - constante de torque
+# --- Parametros do motor ---
+R  = 0.053          # Ohm  - resistencia de armadura
+L  = 0.045e-3       # H    - indutancia de magnetizacao
+M  = 0.0            # H    - indutancia mutua
+Ke = 0.0682         # V/(rad/s) - constante eletrica  (7.14 V/krpm) 
+J  = 1.8e-4         # kg.m^2 - momento de inercia
+B  = 1.2e-6         # coeficiente de amortecimento
+Tl = 0.0            # N.m  - torque de carga
+P  = 14             # numero de pares de polos
+Kt = 0.0682         # N.m/A - constante de torque
 
+# --- Parametros do SVPWM (chaveamento real do inversor) ---
+Fsw        = 10000   # Hz - frequencia de chaveamento
 
-omegacc = 3141
-
+omegacc = Fsw / 10 * 2 * np.pi
 Kpq = L * omegacc
 Kiq = R * omegacc
 
-print(f'Kpq = {Kpq}')
-print(f'Kiq = {Kiq}')
+Kpq = round(Kpq,3)
+Kiq = round(Kiq,3)
 
-omegacs = 314
+omegacs = omegacc / 5 
 Kpomega = J * omegacs / Kt
 Kiomega = J * omegacs**2 / (5*Kt)
 
-print(f'Kpomega = {Kpomega}')
-print(f'Kiomega = {Kiomega}')
+Kpomega = round(Kpomega,3)
+Kiomega = round(Kiomega,3)
+
+print(f'KpOmega = {Kpomega}     # ganho proporcional - malha de velocidade')
+print(f'KiOmega = {Kiomega}     # ganho integral - malha de velocidade')
+print(f'KpId    = {Kpq}         # ganho proporcional - malha de corrente id')
+print(f'KiId    = {Kiq}         # ganho integral - malha de corrente id')
+print(f'KpIq    = {Kpq}         # ganho proporcional - malha de corrente iq')
+print(f'KiIq    = {Kiq}         # ganho integral - malha de corrente iq')
