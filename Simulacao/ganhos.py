c142a11a8db662767e74eaf09167e28a5f25dcf0
andrevale99 <andrefvale@gmail.com>
2026-08-09 21:11:21 -0300

    Novo motor

--- Simulacao/ganhos.py
+++ Simulacao/ganhos.py
@@ -1,15 +1,16 @@
 import numpy as np
 
 # --- Parametros do motor ---
-R  = 0.161           # Ohm  - resistencia de armadura
-L  = 0.052e-3         # H    - indutancia de magnetizacao
+R  = 0.62          # Ohm  - resistencia de armadura
+L  = 8.5e-3       # H    - indutancia de magnetizacao
 M  = 0.0            # H    - indutancia mutua
-Ke = 0.0255     # V/(rad/s) - constante eletrica (2.67 V/krpm)
-J  = 43.3e-3         # kg.m^2 - momento de inercia
-B  = 1.2e-6         # coeficiente de amortecimento
+Ke = 0.69         # V/(rad/s) - constante eletrica  (7.14 V/krpm) 
+J  = 1.8e-4         # kg.m^2 - momento de inercia
+B  = 3.617e-4         # coeficiente de amortecimento
 Tl = 0.0            # N.m  - torque de carga
-P  = 8             # numero de pares de polos
-Kt = 0.0255     # N.m/A - constante de torque
+P  = 4             # numero de pares de polos
+Kt = 0.69         # N.m/A - constante de torque
+
 
 # --- Parametros do SVPWM (chaveamento real do inversor) ---
 Fsw        = 10000   # Hz - frequencia de chaveamento
